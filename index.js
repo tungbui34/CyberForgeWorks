21403e1d9ed758e6612611fb0bebe5a3a0dbe2d9
tungbui34 <malalapapppun0suy@hotmail.com>
2024-05-08 10:20:58 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
+function isValidSudoku(board) {
+  const rows = Array(9)
+    .fill()
+    .map(() => new Set());
+  const cols = Array(9)
+    .fill()
+    .map(() => new Set());
+  const boxes = Array(9)
+    .fill()
+    .map(() => new Set());
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const num = board[i][j];
+      if (num === ".") continue;
+      const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+      if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num))
+        return false;
+      rows[i].add(num);
+      cols[j].add(num);
+      boxes[boxIndex].add(num);
+    }
+  }
+  return true;
+}
